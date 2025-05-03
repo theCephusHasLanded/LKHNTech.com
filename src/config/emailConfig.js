a93a7f84e4b7683478e07cbfb67b93d708becd78
christinacephus@pursuit.org <christinacephus@pursuit.org>
2025-05-03 15:02:42 -0400

    Fix EmailJS template ID
    - Updated template ID to match existing template in EmailJS account
    - Corrected configuration to use an actual template from your account

--- src/config/emailConfig.js
+++ src/config/emailConfig.js
@@ -1,5 +1,5 @@
 // EmailJS configuration
 export const EMAILJS_USER_ID = "christinacephus@gmail.com";
 export const EMAILJS_SERVICE_ID = "service_pz541vs";
-export const EMAILJS_TEMPLATE_ID = "template_kcnxmhc"; // Using the default template from EmailJS
+export const EMAILJS_TEMPLATE_ID = "template_10jkc0t"; // Your actual template ID from EmailJS
 export const EMAILJS_PUBLIC_KEY = "MY-Q5j0BJ5h9LFaH-";
